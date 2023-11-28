f8986d9847e2e925c8969d1ef912b445823fbf96
RodrigoTravessa <rodrigosigiliao@outlook.com>
2023-11-28 16:50:59 -0300

    Isomorphic Strings Using Dictionaries

--- LeetCodeTests/205_IsomorphicStringsTests.cs
+++ LeetCodeTests/205_IsomorphicStringsTests.cs
@@ -1,11 +1,22 @@
+using LeetCode;
+
 namespace LeetCodeTests
+
 {
     public class IsomorphicStringsTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("egg", "add", true)]
+        [InlineData("foo", "bar", false)]
+        [InlineData("paper", "title", true)]
+        [InlineData("ab", "aa", false)]
+        [InlineData("ab", "ca", true)]
+        [InlineData("ab", "ac", true)]
+        [InlineData("ab", "aa", false)]
+        public void Test1(string s, string t, bool res)
         {
-
+            Solution test = new Solution();
+            Assert.Equal(res, test.IsIsomorphic(s, t));
         }
     }
 }
